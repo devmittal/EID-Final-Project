ae6c491c430dc8b7cfa068ce813870b6a573f2fb
Devansh <demi1671@colorado.edu>
2019-11-15 15:50:08 -0700

    Integrated speaker code into main code

--- magic_wand.py
+++ magic_wand.py
@@ -11,6 +11,7 @@ from botocore.exceptions import ClientError
 import json
 import requests
 import os
+import pygame
 
 
 global image_path
@@ -170,6 +171,38 @@ def Recognize_Image():
 
     return json.dumps(index_object["Name"])
     
+def play(filename):
+    """Output audio on speaker
+       Reference: https://raspberrypi.stackexchange.com/questions/7088/playing-audio-files-with-python
+       input: filename - name of audio file to be played""" 
+    
+    print("Playing output")
+    pygame.mixer.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy() == True:
+         pygame.time.Clock().tick(10)
+
+def speak(text, format='ogg_vorbis', voice='Brian'):
+    """Send text to AWS polly and receive converted audio
+       Reference: https://medium.com/@julsimon/johnny-pi-i-am-your-father-part-4-adding-cloud-based-vision-8830c2676113
+       input: text: text to be converted to audio
+              format: format of output audio file
+              voice: AWS voice to output audio file"""
+    
+    polly = boto3.client('polly')
+    
+    filename="/home/pi/EID_SuperProject/Output_Audio/label.ogg"
+    
+    resp = polly.synthesize_speech(OutputFormat=format, Text=text, VoiceId=voice)
+    
+    soundfile = open(filename, 'wb')
+    soundBytes = resp['AudioStream'].read()
+    soundfile.write(soundBytes)
+    soundfile.close()
+    play(filename)
+    os.remove(filename)
+    
 if __name__ == '__main__':
 	client = boto3.client('rekognition')
 	
@@ -178,5 +211,7 @@ if __name__ == '__main__':
 	upload_file(wav_output_filename, bucket_name)
 	if IsAudioTranscriptionSuccess(wav_output_filename, bucket_name):
 		Capture_Image()
-		print(Recognize_Image())
+        image_label = Recognize_Image() + "is the object identifieddd"
+		print(image_label)
+        speak(image_label)
     
