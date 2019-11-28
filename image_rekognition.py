329c84ba00331b0db92f4c981cd565480c6d91c5
Devansh <demi1671@colorado.edu>
2019-11-28 11:32:40 -0700

    Added code to upload image captured to s3 bucket

--- image_rekognition.py
+++ image_rekognition.py
@@ -2,16 +2,17 @@
 
 from picamera import PiCamera
 from time import sleep
+import logging
 import boto3
 import json
 import os
-
 
 camera = PiCamera()
 camera.rotation = 180
 camera.resolution = (1296,972)
 
 image_path = "images/object.jpg"
+image_bucket = 'eid-superproject-image'
 
 if os.path.exists(image_path):
 	os.remove(image_path)
@@ -25,9 +26,15 @@ camera.capture(image_path)
 camera.stop_preview()
 
 client = boto3.client('rekognition')
+s3_client = boto3.client('s3')
 
 with open(image_path, 'rb') as image:
 	image_stream = image.read()
+	
+try:
+	resp = s3_client.upload_file(image_path, image_bucket, image_path)
+except ClientError as e:
+	logging.error(e)
 	
 response = client.detect_labels(Image={'Bytes':image_stream},
 								MaxLabels=1)
