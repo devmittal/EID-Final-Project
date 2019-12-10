fa9e4014155f7ea354ca75fab4565beb89aa09ba
Souvik De <souvik.de@colorado.edu>
2019-12-09 23:08:12 -0700

    Removed Bugs

--- server_test.py
+++ server_test.py
@@ -24,41 +24,44 @@ def GetSQSQueueData():
 
     queue = GetSQSQueueObject()
 
-    # Process messages by printing out body and optional author name
-    for message in queue.receive_messages():
-        # Print out the body and author (if set)
-        print('Message -> {0}'.format(message.body))
+    while(1):
+        # Process messages by printing out body and optional author name
+        for message in queue.receive_messages():
+            # Print out the body and author (if set)
+            print('Message -> {0}'.format(message.body))
 
-        if count == 0:
-            if message.body == "identify.":
-                DAL.InsertToCommand(message.body, 'Yes')
+            if count == 0:
+                if message.body == "identify.":
+                    DAL.InsertToCommand(message.body, 'Yes')
+                    count += 1
+                else:
+                    DAL.InsertToCommand(message.body, 'No')
+                    count = 0
+            elif message.body == "image":
+                print('Dowloading image from s3')
+                s3.download_file(image_bucket, object_name, downloaded_image)
+                count += 1
+            elif count == 2:
+                label = message.body
                 count += 1
             else:
-                DAL.InsertToCommand(message.body, 'No')
-                count = 0
-        elif message.body == "image":
-            print('Dowloading image from s3')
-            s3.download_file(image_bucket, object_name, downloaded_image)
-            count += 1
-        elif count == 2:
-            label = message.body
-            count += 1
-        else:
-            confirmation = message.body
-            if confirmation == "correct":
-                DAL.InsertToObject(label, "correct")
-                DAL.InsertToCommand(confirmation, "Yes")
-            elif confirmation == "wrong":
-                DAL.InsertToObject(label, "wrong")
-                DAL.InsertToCommand(confirmation, "Yes")
-            else:
-                DAL.InsertToObject(label, "unclear")
-                DAL.InsertToCommand(confirmation, "No")
-            count += 1
-        # Let the queue know that the message is processed
-        message.delete()
+                confirmation = message.body
+                if confirmation == "correct":
+                    DAL.InsertToObject(label, "correct")
+                    DAL.InsertToCommand(confirmation, "Yes")
+                elif confirmation == "wrong":
+                    DAL.InsertToObject(label, "wrong")
+                    DAL.InsertToCommand(confirmation, "Yes")
+                else:
+                    DAL.InsertToObject(label, "unclear")
+                    DAL.InsertToCommand(confirmation, "No")
+                count += 1
+            # Let the queue know that the message is processed
+            message.delete()
 
-        if count == 4:
-            return label
+            count = count % 4
+
+            if label != "":
+                return label
 
 GetSQSQueueData()
