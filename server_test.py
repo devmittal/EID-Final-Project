4063e52823c5092f981a050d661b48940290ccc1
Souvik De <souvik.de@colorado.edu>
2019-12-09 22:56:31 -0700

    New Server Code under Test

--- server_test.py
+++ server_test.py
@@ -3,21 +3,27 @@
 import boto3
 import DAL
 
-count = 0
-label = ""
-confirmation = ""
-image_bucket='eid-superproject-image'
-object_name='images/object.jpg'
-downloaded_image='downloaded_images/image.jpg'
+global sqs
+global s3
 
 # Get the service resource
 sqs = boto3.resource('sqs')
 s3 = boto3.client('s3')
 
 # Get the queue
-queue = sqs.get_queue_by_name(QueueName='Magic-Wand.fifo')
+def GetSQSQueueObject():
+    return sqs.get_queue_by_name(QueueName='Magic-Wand.fifo')
 
-while(1):
+def GetSQSQueueData():
+    count = 0
+    label = ""
+    confirmation = ""
+    image_bucket = 'eid-superproject-image'
+    object_name = 'images/object.jpg'
+    downloaded_image = 'downloaded_images/image.jpg'
+
+    queue = GetSQSQueueObject()
+
     # Process messages by printing out body and optional author name
     for message in queue.receive_messages():
         # Print out the body and author (if set)
@@ -26,20 +32,31 @@ while(1):
         if count == 0:
             if message.body == "identify.":
                 DAL.InsertToCommand(message.body, 'Yes')
+                count += 1
             else:
                 DAL.InsertToCommand(message.body, 'No')
+                count = 0
         elif message.body == "image":
             print('Dowloading image from s3')
             s3.download_file(image_bucket, object_name, downloaded_image)
+            count += 1
         elif count == 2:
             label = message.body
+            count += 1
         else:
             confirmation = message.body
             if confirmation == "correct":
-                DAL.InsertToObject(label, "Yes")
+                DAL.InsertToObject(label, "correct")
                 DAL.InsertToCommand(confirmation, "Yes")
-            elif confirmation == "wrong"
+            elif confirmation == "wrong":
+                DAL.InsertToObject(label, "wrong")
+                DAL.InsertToCommand(confirmation, "Yes")
+            else:
+                DAL.InsertToObject(label, "unclear")
+                DAL.InsertToCommand(confirmation, "No")
+            count += 1
         # Let the queue know that the message is processed
         message.delete()
 
-        count += 1
+        if count == 4:
+            return label
